aad71a0a4b3e9e87d4b239b0d800ae01099f8d88
vnurhaqiqi <viqinurhaqiqi@gmail.com>
2022-02-18 21:53:04 +0700

    inital commit

---
+++ channel_test.go
@@ -0,0 +1,61 @@
+package gogoroutines
+
+import (
+	"fmt"
+	"testing"
+	"time"
+)
+
+func TestCreateChannel(t *testing.T) {
+	channel := make(chan string)
+	defer close(channel)
+
+	go func() {
+		time.Sleep(2 * time.Second)
+		channel <- "Viqi Nurhaqiqi"
+
+		fmt.Println("Selesai mengirim data ke Channel")
+	}()
+
+	data := <-channel
+	fmt.Println(data)
+
+	time.Sleep(5 * time.Second)
+}
+
+func GiveMeResponse(channel chan string) {
+	time.Sleep(2 * time.Second)
+	channel <- "Viqi Nurhaqiqi"
+}
+
+func TestChannelParameter(t *testing.T) {
+	channel := make(chan string)
+	defer close(channel)
+
+	go GiveMeResponse(channel)
+
+	data := <-channel
+	fmt.Println(data)
+
+	time.Sleep(5 * time.Second)
+}
+
+func OnlyIn(channel chan<- string) {
+	time.Sleep(2 * time.Second)
+	channel <- "Viqi Nurhaqiqi"
+}
+
+func OnlyOut(channel <- chan string) {
+	data := <-channel
+	fmt.Println(data)
+}
+
+func TestChannelInOut(t *testing.T) {
+	channel := make(chan string)
+	defer close(channel)
+
+	go OnlyIn(channel)
+	go OnlyOut(channel)
+
+	time.Sleep(5 * time.Second)
+}
